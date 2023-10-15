6dc1a237965029020ef1283a323c46ebc58678b4
Ode42 <ode.progr@outlook.com>
2023-10-15 13:31:34 +0300

    Flashcard api endpoint

--- src/db/db.sql
+++ src/db/db.sql
@@ -20,10 +20,12 @@ CREATE TABLE flashcards (
     card_key TEXT NOT NULL,
     card_description TEXT NOT NULL,
     card_date DATE NOT NULL,
-    card_set SERIAL NOT NULL /* id of the card's set */
+    card_set SERIAL NOT NULL, /* id of the card's set */
+    card_owner SERIAL NOT NULL
 );
  /*
-INSERT INTO flashcards (card_key, card_description, card_date, card_set) VALUES ('testi2', 'vastaus2', '2023-10-15', 1);
-INSERT INTO flashcards (card_key, card_description, card_date, card_set) VALUES ('testi3', 'vastaus3', '2023-10-15', 2);
-INSERT INTO flashcards (card_key, card_description, card_date, card_set) VALUES ('testi4', 'vastaus4', '2023-10-15', 2);
+INSERT INTO flashcards (card_key, card_description, card_date, card_set, card_owner) VALUES ('testi1', 'vastaus1', '2023-10-15', 1, 1);
+INSERT INTO flashcards (card_key, card_description, card_date, card_set, card_owner) VALUES ('testi2', 'vastaus2', '2023-10-15', 1, 1);
+INSERT INTO flashcards (card_key, card_description, card_date, card_set, card_owner) VALUES ('testi4', 'vastaus4', '2023-10-15', 2, 1);
+INSERT INTO flashcards (card_key, card_description, card_date, card_set, card_owner) VALUES ('testi3', 'vastaus3', '2023-10-15', 2, 1);
 */
--- src/router/cards-routes.js
+++ src/router/cards-routes.js
@@ -8,7 +8,18 @@ const router = express.Router();
 router.get('/sets', authenticateToken, async (request, response) => {
   const userId = request.user.user_id;
   const sets = await pool.query('SELECT * FROM sets WHERE set_owner = $1', [userId]);
-  console.log(sets.rows);
+  const cards = await pool.query('SELECT * FROM flashcards WHERE card_owner = $1', [userId]);
+  const cardsets = sets.rows.map((set, i) => {
+    return {
+      set_id: set.set_id,
+      set_name: set.set_name,
+      set_description: set.set_description,
+      set_date: set.set_date,
+      set_owner: set.set_owner,
+      set_cards: cards.rows.filter((card) => card.card_set == set.set_id)
+    };
+  });
+  response.json(cardsets).status(200);
 });
 
 export default router;
